93341d5757498e03d5c6b695f08ef4178efeaeaf
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-04-30 16:11:11 +0300

    task-3

--- src/requests/products.js
+++ src/requests/products.js
@@ -9,3 +9,8 @@ export async function getProductById(id) {
   const response = await instance.get(`/products/${id}`);
   return response.data;
 }
+
+export async function addNewProduct(data) {
+  const response = await instance.post("/products/add", data);
+  return response.data;
+}
